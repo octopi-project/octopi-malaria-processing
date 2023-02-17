be0e20cf04757271d0eb75ff0b5ff6098f31a3c7
Hongquan Li <hqlisu@gmail.com>
2023-02-17 10:22:21 -0800

    add model evaluation outside the GUI + utils for exporting movies

---
+++ interactive annotator/utils_visualization.py
@@ -0,0 +1,27 @@
+import imageio
+import numpy as np
+import cv2
+
+def make_movie(images_numpy, output_file, scale_factor=5, fps=24):
+
+    writer = imageio.get_writer(output_file, fps=fps)
+
+    for i in range(len(images_numpy)):
+        frame = images_numpy[i]
+        frame = frame.transpose(1,2,0)
+        img_fluorescence = frame[:,:,[2,1,0]]
+        img_dpc = frame[:,:,3]
+        img_dpc = np.dstack([img_dpc,img_dpc,img_dpc])
+        img_overlay = 0.64*img_fluorescence + 0.36*img_dpc
+        frame = np.hstack([img_dpc,img_fluorescence,img_overlay]).astype('uint8')
+        # print(frame.shape)
+        # print(np.amax(frame))
+
+        new_height, new_width = int(frame.shape[0] * scale_factor), int(frame.shape[1] * scale_factor)
+        frame = cv2.resize(frame,(new_width, new_height),interpolation=cv2.INTER_NEAREST)
+        writer.append_data(frame)
+        print(i)
+        if i > 200:
+            break
+
+    writer.close()
