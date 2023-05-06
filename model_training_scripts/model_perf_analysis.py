08c8da773dc12e35a88ea86bd351b41390d7d49d
rinnibhan <rinbha@stanford.edu>
2023-05-05 17:53:07 -0700

    wrote pseudocode for what I need to do

--- model_training_scripts/model_perf_analysis.py
+++ model_training_scripts/model_perf_analysis.py
@@ -3,24 +3,8 @@ import pandas as pd
 import os
 
 ''' 
-want to take in ann with predictions and:
-if 2 class classifier, I want a few things:
-- a = frac of unsure labeled pos
-- b = frac of pos labeled pos
-- c = frac of neg labeled pos
-number of pos, neg, uns: d, e, f
-also, if I want to run the analysis on the relabeled annotations, I can sub out the annotations column in my file with the annotations column in the relabeled file (should be in the same order!)
-then, I want:
-- b_1 = frac of pos labeled pos in relabeled
-- c_1 = frac of neg labeled pos in relabeled
-number of pos, neg in relabeled: d_1, e_1
-as I vary the positive threshold (the value t at which pred > t means we predict positive)
-note that:
-- FPR = (a + c)/(e) or c_1/(e_1)
-- TPR = b/d or b_1/d_1
-plot ROC: FPR (x) vs TPR (y)
-
-for a given threshold, calculate:
+Binary classifier:
+for a given threshold, (the value t at which para > t means we predict positive) calculate:
 - a = frac of unsure labeled pos
 - b = frac of pos labeled pos
 - c = frac of neg labeled pos
@@ -38,3 +22,22 @@ for a given threshold, calculate:
 - TN
 - Sensitivity
 - Specificity
+
+Ternary classifier:
+One way:
+Pick a threshold again (the value t at which para pred > t means we predict positive)
+For all other images, predict negative
+Then calculate the same things!
+Other way:
+check that link, but can do the one vs rest strategy above for all classes^!
+
+pseudocode:
+make df where rows are for each new thresh t and cols are values of interest
+define it with column names^ (thres, TN, TP, ..., specificity or something)
+begin for loop, running through thresh t
+read in csv
+get np array of predictions based on thresh t
+compare predictions to ground truth
+get TN, TP, FN, FP
+calculate all other values accordingly
+fill into df as you go
